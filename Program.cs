7d9eb2f2ff8cfd21be4bc2185a982555244cc685
Bashar Abdelqader <bashara@microsoft.com>
2024-04-25 10:07:39 -0700

    Addeded application insight

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+var options = new ApplicationInsightsServiceOptions
+{
+    ConnectionString = "InstrumentationKey=1f45fcf6-5d8b-47ee-b7e9-1a1a17b593e1;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=d05650e1-667b-4ab0-8f21-de43edd26085"
+};
+builder.Services.AddApplicationInsightsTelemetry(options);
 
 var app = builder.Build();
 
